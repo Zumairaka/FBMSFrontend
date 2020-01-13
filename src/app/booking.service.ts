52d05253d3dea9a9384b061c4610c85883d49e3b
Zumairaka <zumairaka@gmail.com>
2020-01-13 21:54:13 +0530

    first

--- src/app/booking.service.ts
+++ src/app/booking.service.ts
@@ -11,71 +11,71 @@ export class BookingService {
   constructor(private http: HttpClient, private router: Router) { }
 
   signUpData(user) {
-    return this.http.post('http://localhost:3000/signup', user);
+    return this.http.post('https://fbms-backend.herokuapp.com/signup', user);
   }
 
   editAccountData(user) {
-    return this.http.post('http://localhost:3000/signup/edit', user);
+    return this.http.post('https://fbms-backend.herokuapp.com/signup/edit', user);
   }
 
   loginVerify(user) {
-    return this.http.post('http://localhost:3000/login', user);
+    return this.http.post('https://fbms-backend.herokuapp.com/login', user);
   }
 
   checkAvailable(bookingData) {
-    return this.http.post('http://localhost:3000/booking/check', bookingData);
+    return this.http.post('https://fbms-backend.herokuapp.com/booking/check', bookingData);
   }
 
   saveMyGame(bookingData) {
-    return this.http.post('http://localhost:3000/booking/save', bookingData);
+    return this.http.post('https://fbms-backend.herokuapp.com/booking/save', bookingData);
   }
 
   getBookingDataToday(today) {
-    return this.http.post('http://localhost:3000/booking/dataToday', {date: today});
+    return this.http.post('https://fbms-backend.herokuapp.com/booking/dataToday', {date: today});
   }
 
   getBookingData() {
-    return this.http.get('http://localhost:3000/booking');
+    return this.http.get('https://fbms-backend.herokuapp.com/booking');
   }
 
   getMyBookingData(name) {
-    return this.http.post('http://localhost:3000/booking/myData', {uname: name});
+    return this.http.post('https://fbms-backend.herokuapp.com/booking/myData', {uname: name});
   }
 
   getUserData(uname) {
-    return this.http.post('http://localhost:3000/signup/getUser', {name: uname});
+    return this.http.post('https://fbms-backend.herokuapp.com/signup/getUser', {name: uname});
   }
 
   getUsersData() {
-    return this.http.get('http://localhost:3000/signup/getUsers');
+    return this.http.get('https://fbms-backend.herokuapp.com/signup/getUsers');
   }
 
   deleteBooking(name, day, tym) {
-    return this.http.post('http://localhost:3000/booking/delete', {uname: name, date: day, time: tym});
+    return this.http.post('https://fbms-backend.herokuapp.com/booking/delete', {uname: name, date: day, time: tym});
   }
 
   deleteAllBooking(uname) {
-    return this.http.post('http://localhost:3000/booking/deleteAll', {name: uname});
+    return this.http.post('https://fbms-backend.herokuapp.com/booking/deleteAll', {name: uname});
   }
 
   deleteUser(uname) {
-    return this.http.post('http://localhost:3000/signup/delete', {name: uname});
+    return this.http.post('https://fbms-backend.herokuapp.com/signup/delete', {name: uname});
   }
 
   saveEvent(eventData) {
-    return this.http.post('http://localhost:3000/event', eventData);
+    return this.http.post('https://fbms-backend.herokuapp.com/event', eventData);
   }
 
   getEventData() {
-    return this.http.get('http://localhost:3000/event/getEvents');
+    return this.http.get('https://fbms-backend.herokuapp.com/event/getEvents');
   }
 
   getEvent(name, day, tym) {
-    return this.http.post('http://localhost:3000/event/getEvent', {ename: name, date: day, time: tym});
+    return this.http.post('https://fbms-backend.herokuapp.com/event/getEvent', {ename: name, date: day, time: tym});
   }
 
   deleteEvent(name, day, tym) {
-    return this.http.post('http://localhost:3000/event/delete', {ename: name, date: day, time: tym});
+    return this.http.post('https://fbms-backend.herokuapp.com/event/delete', {ename: name, date: day, time: tym});
   }
 
 
